a3a5dce4cf11b8ecb0ed8c0a405a31930d601f04
rkr-dev <rinil.kunhiraman@protonmail.com>
2022-01-05 23:07:00 -0800

    Basic Server Setup
     - Installed Dependencies
     - env setup

---
+++ server.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello from the root route' })
+})
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => {
+  console.log(`Server started running on port ${PORT}`)
+})
+
+process.on('UnhandledRejection', (err, promise) => {
+  console.log(`Error occured ${err.message}`)
+  server.close(() => process.exit(1))
+})
